4c7185f07afcbdf28c7b882919ac938ae0d2d041
Fraz Shabbir <fraz.shabbir54@gmail.com>
2023-01-25 23:56:09 +0500

    updated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
             $user->username = 'admin';
             $user->status = 1;
             $user->password = bcrypt('admin');
-            $user->email = 'admin@wisevision.dev';
+            $user->email = 'fraz.shabbir54@gmail.com';
             $user->email_verified_at = now();
             // $user->ar_user = true;
             $user->assignRole('Super Admin');
