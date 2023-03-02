4df9d15e493b78a4e5c32c054be80d58ac19372c
Fraz Shabbir <fraz.shabbir54@gmail.com>
2023-03-02 12:55:50 +0500

    Update main.blade.php

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -2,55 +2,72 @@
 <html lang="en">
 
 <!-- Mirrored from themeht.com/template/vCards/ltr/index-4.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 25 Jan 2023 17:25:34 GMT -->
+
 <head>
-@include('frontend.partial._header')
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3329597755027709"
+        crossorigin="anonymous"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VHCMR9NB1F"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-VHCMR9NB1F');
+    </script>
+    
+    @include('frontend.partial._header')
 </head>
 
 <body>
 
-<!-- page wrapper start -->
+    <!-- page wrapper start -->
 
-<div class="page-wrapper">
-  
-<!-- preloader start -->
+    <div class="page-wrapper">
 
-@include('frontend.partial._preloader')
+        <!-- preloader start -->
 
-<!-- preloader end -->
+        @include('frontend.partial._preloader')
 
-<!--header start-->
-@yield('banner')
+        <!-- preloader end -->
 
-
-<!--body content start-->
-
-<div class="page-content">
-
-<!--feature start-->
-@yield('content')
+        <!--header start-->
+        @yield('banner')
 
 
+        <!--body content start-->
 
-</div>
+        <div class="page-content">
 
-<!--body content end--> 
+            <!--feature start-->
+            @yield('content')
 
 
-<!--footer start-->
 
-@include('frontend.partial._footer')
+        </div>
 
-<!--footer end-->
+        <!--body content end-->
 
-</div>
 
-<!-- page wrapper end -->
+        <!--footer start-->
 
-@include('frontend.partial._scripts')
-@include('sweetalert::alert')
+        @include('frontend.partial._footer')
+
+        <!--footer end-->
+
+    </div>
+
+    <!-- page wrapper end -->
+
+    @include('frontend.partial._scripts')
+    @include('sweetalert::alert')
 
 </body>
 
 
 <!-- Mirrored from themeht.com/template/vCards/ltr/index-4.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 25 Jan 2023 17:30:09 GMT -->
+
 </html>
