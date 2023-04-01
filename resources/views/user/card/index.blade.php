ab17bf030fb38b90f8d93488be89abb6e1b0f7bd
Fraz Shabbir <fraz.shabbir54@gmail.com>
2023-04-01 13:19:57 +0400

    Update index.blade.php

--- resources/views/user/card/index.blade.php
+++ resources/views/user/card/index.blade.php
@@ -84,30 +84,30 @@
                                             
                                             <tr>
                                                 <td>type</td>
-                                                <td><span class="badge badge-{{ auth()->user()->order->user->type }}">{{ ucfirst(auth()->user()->order->user->type) }}</span></td>
+                                                <td><span class="badge badge-{{ auth()->user()->type }}">{{ ucfirst(auth()->user()->order->user->type) }}</span></td>
                                             </tr>
                                             <tr>
                                                 <td>Name</td>
-                                                <td>{{ getFullname(auth()->user()->order->user->id) }}</td>
+                                                <td>{{ getFullname(auth()->user()->id) }}</td>
 
                                             </tr>
 
                                             <tr>
                                                 <td>Username</td>
-                                                <td>{{ auth()->user()->order->user->username }}</td>
+                                                <td>{{ auth()->user()->username }}</td>
 
                                             </tr>
 
 
                                             <tr>
                                                 <td>Email</td>
-                                                <td>{{ auth()->user()->order->user->email }}</td>
+                                                <td>{{ auth()->user()->email }}</td>
 
                                             </tr>
 
                                             <tr>
                                                 <td>Phone</td>
-                                                <td>{{ auth()->user()->order->user->phone }}</td>
+                                                <td>{{ auth()->user()->phone }}</td>
 
                                             </tr>
 
