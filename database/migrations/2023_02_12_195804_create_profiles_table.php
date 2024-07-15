1eacfffc58b2dbb2eacb82420cf27257d1310441
Fraz Shabbir <fraz.shabbir54@gmail.com>
2024-07-15 18:12:37 +0100

    Update 2023_02_12_195804_create_profiles_table.php

--- database/migrations/2023_02_12_195804_create_profiles_table.php
+++ database/migrations/2023_02_12_195804_create_profiles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->string('bio')->nullable();
+            $table->text('bio')->nullable();
             $table->string('organization')->nullable();
             $table->string('designation')->nullable();
             $table->string('avatar')->default('default/avatar/default.png');
